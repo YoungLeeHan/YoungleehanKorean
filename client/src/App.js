667fa940f242e60dbd4cc095501e9da70f373f21
Young <hyangyoungs@gmail.com>
2023-07-27 17:49:54 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./styles/App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 import Header from "../src/components/nav/Header";
 import Footer from "../src/components/nav/Footer";
 import Home from "./pages/Home";
@@ -25,67 +25,54 @@ import Cart from "./pages/Cart";
 import ProductsView from "./pages/Shop/ProductsView";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Toaster />
-      <Routes>
-        {/*single page*/}
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<PageNotFound />} />
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Toaster/>
+            <Routes>
+                {/*single page*/}
+                <Route path="/" element={<Home/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
 
-        {/*login*/}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/verification" element={<Verification />} />
-        <Route path="/newpassword" element={<NewPassword />} />
-          
-        {/*shop*/}
-        <Route path="/shop" element={<ProductsView />} />
-        <Route path="/shop/:slug" /> 
+                {/*login*/}
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/forgotpassword" element={<ForgotPassword/>}/>
+                <Route path="/verification" element={<Verification/>}/>
+                <Route path="/newpassword" element={<NewPassword/>}/>
 
-<<<<<<< HEAD
                 {/*shop*/}
-                <Route path="/shop" element={<ProductsView />} />
-                <Route path="/shop/:slug" />
-
-                {/*Cart & Payment*/}
-                <Route path="/cart" />
-                <Route path="/cart/payment" />
+                <Route path="/shop" element={<ProductsView/>}/>
+                <Route path="/shop/:slug"/>
 
                 {/*blog*/}
-                <Route path="/blog" element={<BlogView />} />
-                <Route path="/blog/:_id" />
-                <Route path="/blog/upload" element={<Upload />} />
-=======
-        {/*Cart & Payment*/}
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/cart/payment" /> 
+                <Route path="/blog" element={<BlogView/>}/>
+                <Route path="/blog/:_id"/>
+                <Route path="/blog/upload" element={<Upload/>}/>
 
-        {/*blog*/}
-        <Route path="/blog" element={<List />} />
-        <Route path="/blog/:_id"/>
-        <Route path="/blog/upload" element={<Upload />} />
+                {/*Cart & Payment*/}
+                <Route path="/cart" element={<Cart/>}/>
+                <Route path="/cart/payment"/>
 
-        {/*user dashboard*/}
-        <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="user" element={<Dashboard />} />
-          <Route path="user/profile" element={<UserProfile />} />
-          <Route path="user/orders" element={<UserOrders />} />
-        </Route>
->>>>>>> 019f8aa291b1c32d010e74bde9f83c9b65598845
+                {/*user dashboard*/}
+                <Route path="/dashboard" element={<PrivateRoute/>}>
+                    <Route path="user" element={<Dashboard/>}/>
+                    <Route path="user/profile" element={<UserProfile/>}/>
+                    <Route path="user/orders" element={<UserOrders/>}/>
+                </Route>
 
-        {/*admin dashboard*/}
-        <Route path="/dashboard" element={<AdminRoute />}>
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="admin/blog" element={<AdminBlog />} />
-          <Route path="admin/product" element={<AdminProduct />} />
-        </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+                {/*admin dashboard*/}
+                <Route path="/dashboard" element={<AdminRoute/>}>
+                    <Route path="admin" element={<AdminDashboard/>}/>
+                    <Route path="admin/blog" element={<AdminBlog/>}/>
+                    <Route path="admin/product" element={<AdminProduct/>}/>
+                </Route>
+
+
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    );
 
 }
