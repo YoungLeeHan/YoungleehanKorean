dd3a0d82a1261ca923d734227854725e19cb1cf6
Younghyun Lee <younghyunlee22@gmail.com>
2023-07-27 15:53:15 -0500

    braintree setup

--- client/src/App.js
+++ client/src/App.js
@@ -14,53 +14,55 @@ import Verification from "./pages/Account/Verification";
 import NewPassword from "./pages/Account/NewPassword";
 import Contact from "./pages/Contact";
 import Dashboard from "./pages/User/Dashboard";
-import PrivateRoute from "./components/routes/PrivateRoute"
+import PrivateRoute from "./components/routes/PrivateRoute";
 import AdminDashboard from "./pages/Admin/Dashboard";
 import AdminRoute from "./components/routes/AdminRoute";
 import AdminBlog from "./pages/Admin/Blog";
 import AdminProduct from "./pages/Admin/Product";
 import UserOrders from "./pages/User/Orders";
 import UserProfile from "./pages/User/Profile";
+import Cart from "./pages/Cart";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Toaster />
-            <Routes>
-                {/*single page*/}
-                <Route path="/" element={<Home />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<PageNotFound />} />
+  return (
+    <BrowserRouter>
+      <Header />
+      <Toaster />
+      <Routes>
+        {/*single page*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
 
-                {/*login*/}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/forgotpassword" element={<ForgotPassword />} />
-                <Route path="/verification" element={<Verification />} />
-                <Route path="/newpassword" element={<NewPassword />} />
+        {/*login*/}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/verification" element={<Verification />} />
+        <Route path="/newpassword" element={<NewPassword />} />
 
-                {/*blog*/}
-                <Route path="/blog" element={<List />} />
-                <Route path="/blog/upload" element={<Upload />} />
+        {/*cart*/}
+        <Route path="/cart" element={<Cart />} />
 
-                {/*user dashboard*/}
-                <Route path="/dashboard" element={<PrivateRoute />}>
-                    <Route path="user" element={<Dashboard />} />
-                    <Route path="user/profile" element={<UserProfile />} />
-                    <Route path="user/orders" element={<UserOrders/>} />
-                </Route>
+        {/*blog*/}
+        <Route path="/blog" element={<List />} />
+        <Route path="/blog/upload" element={<Upload />} />
 
-                {/*admin dashboard*/}
-                <Route path="/dashboard" element={<AdminRoute />}>
-                    <Route path="admin" element={<AdminDashboard />} />
-                    <Route path="admin/blog" element={<AdminBlog />} />
-                    <Route path="admin/product" element={<AdminProduct />} />
-                </Route>
+        {/*user dashboard*/}
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/profile" element={<UserProfile />} />
+          <Route path="user/orders" element={<UserOrders />} />
+        </Route>
 
-
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    );
+        {/*admin dashboard*/}
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/blog" element={<AdminBlog />} />
+          <Route path="admin/product" element={<AdminProduct />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
