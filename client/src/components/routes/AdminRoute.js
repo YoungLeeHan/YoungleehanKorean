422e2147bf4faa6818499e01364909584c3aa788
Younghyun Lee <younghyunlee22@gmail.com>
2023-07-28 17:51:37 -0500

    braintree drop-in

--- client/src/components/routes/AdminRoute.js
+++ client/src/components/routes/AdminRoute.js
@@ -1,28 +1,27 @@
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useAuth } from "../../context/auth";
-import Loading from '../routes/Loading'
+import Loading from "../routes/Loading";
 import axios from "axios";
 
 export default function AdminRoute() {
-    // context
-    const [auth, setAuth] = useAuth();
-    // state
-    const [ok, setOk] = useState(false);
+  // context
+  const [auth, setAuth] = useAuth();
+  // state
+  const [ok, setOk] = useState(false);
 
-    useEffect(() => {
-        const adminCheck = async () => {
-            const { data } = await axios.get(`/admin-check`);
-            if (data.ok) {
-                setOk(true);
-            } else {
-                setOk(false);
-            }
-        };
+  useEffect(() => {
+    const adminCheck = async () => {
+      const { data } = await axios.get(`/admin-check`);
+      if (data.ok) {
+        setOk(true);
+      } else {
+        setOk(false);
+      }
+    };
 
-        if (auth?.token) adminCheck();
-    }, [auth?.token]);
+    if (auth?.token) adminCheck();
+  }, [auth?.token]);
 
-
-    return ok ? <Outlet /> : <Loading path="" />;
+  return ok ? <Outlet /> : <Loading path="" />;
 }
