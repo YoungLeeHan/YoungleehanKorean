3758ee4539ad78124140301184df46b6cd90726a
Danbi Choi <sweetrain05@hotmail.com>
2023-08-01 12:10:56 -0700

    Cart Page : Product card componentized

---
+++ client/src/components/cards/CartProductCard.js
@@ -0,0 +1,74 @@
+// 👻 Developed by DanBi Choi on Aug 1st, 2023.
+// -----------------------------------------------------
+
+import { TiDelete } from "react-icons/ti";
+import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+export default function CartProductCard({
+    item,
+    cartQuantity,
+    handleDelete,
+    handleQuantityChange,
+}) {
+    <tr key={item?._id}>
+        <td className="product-info">
+            <Link to={`/shop/${item?.slug}`}>
+                <div className="img">
+                    <img
+                        src={`${process.env.REACT_APP_API}/product/images/${item._id}`}
+                        alt={item?.title}
+                    />
+                </div>
+                <h3>{item?.title}</h3>{" "}
+            </Link>
+        </td>
+
+        <td>
+            <h4>${item?.price}</h4>
+        </td>
+        <td className="td-quantity">
+            <div className="control-box d-flex flex-row justify-content-end align-items-center">
+                <h4>{cartQuantity[item._id]}</h4>
+                <div className="arrow-box d-flex flex-column">
+                    <button
+                        className="arrow-btn"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleQuantityChange("up", item._id);
+                        }}
+                    >
+                        <BiSolidUpArrow size="10px" fill="#B4B1B1" />
+                    </button>
+                    <button
+                        className="arrow-btn"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleQuantityChange("down", item._id);
+                        }}
+                        disabled={cartQuantity[item._id] === 1}
+                    >
+                        <BiSolidDownArrow size="10px" fill="#B4B1B1" />
+                    </button>
+                </div>
+            </div>
+        </td>
+        <td>
+            <h4>
+                <span>
+                    ${(item?.price * cartQuantity[item._id]).toFixed(2)}
+                </span>
+            </h4>
+        </td>
+        <td>
+            <button
+                className="delete-btn"
+                onClick={(e) => {
+                    handleDelete(item._id);
+                }}
+            >
+                <TiDelete fill="#ffbf35" size={20} />
+            </button>
+        </td>
+    </tr>;
+}
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,13 +5,12 @@ import Jumbotron from "../components/cards/Jumbotron";
 import ScrollToTop from "../components/nav/ScrollToTop";
 import { useCart } from "../context/cart";
 import { useCartQuantity } from "../context/cartQuantity";
-import { TiDelete } from "react-icons/ti";
 import { Link, useNavigate } from "react-router-dom";
-import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
 import { useEffect } from "react";
 import { useAuth } from "../context/auth";
 import { toast } from "react-hot-toast";
 import { useCartTotal } from "../context/cartTotal";
+import CartProductCard from "./../components/cards/CartProductCard";
 
 export default function Cart() {
     ScrollToTop();
@@ -89,93 +88,12 @@ export default function Cart() {
                                 <th> </th>
                             </tr>
                             {cart?.map((item) => {
-                                return (
-                                    <tr key={item?._id}>
-                                        <td className="product-info">
-                                            <Link to={`/shop/${item?.slug}`}>
-                                                <div className="img">
-                                                    <img
-                                                        src={`${process.env.REACT_APP_API}/product/images/${item._id}`}
-                                                        alt={item?.title}
-                                                    />
-                                                </div>
-                                                <h3>{item?.title}</h3>{" "}
-                                            </Link>
-                                        </td>
-
-                                        <td>
-                                            <h4>${item?.price}</h4>
-                                        </td>
-                                        <td className="td-quantity">
-                                            <div className="control-box d-flex flex-row justify-content-end align-items-center">
-                                                <h4>
-                                                    {cartQuantity[item._id]}
-                                                </h4>
-                                                <div className="arrow-box d-flex flex-column">
-                                                    <button
-                                                        className="arrow-btn"
-                                                        onClick={(e) => {
-                                                            e.preventDefault();
-                                                            handleQuantityChange(
-                                                                "up",
-                                                                item._id
-                                                            );
-                                                        }}
-                                                    >
-                                                        <BiSolidUpArrow
-                                                            size="10px"
-                                                            fill="#B4B1B1"
-                                                        />
-                                                    </button>
-                                                    <button
-                                                        className="arrow-btn"
-                                                        onClick={(e) => {
-                                                            e.preventDefault();
-                                                            handleQuantityChange(
-                                                                "down",
-                                                                item._id
-                                                            );
-                                                        }}
-                                                        disabled={
-                                                            cartQuantity[
-                                                                item._id
-                                                            ] === 1
-                                                        }
-                                                    >
-                                                        <BiSolidDownArrow
-                                                            size="10px"
-                                                            fill="#B4B1B1"
-                                                        />
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <h4>
-                                                <span>
-                                                    $
-                                                    {(
-                                                        item?.price *
-                                                        cartQuantity[item._id]
-                                                    ).toFixed(2)}
-                                                </span>
-                                            </h4>
-                                        </td>
-                                        <td>
-                                            <button
-                                                className="delete-btn"
-                                                onClick={(e) => {
-                                                    handleDelete(item._id);
-                                                }}
-                                            >
-                                                <TiDelete
-                                                    fill="#ffbf35"
-                                                    size={20}
-                                                />
-                                            </button>
-                                        </td>
-                                    </tr>
-                                );
+                                <CartProductCard
+                                    item={item}
+                                    cartQuantity={cartQuantity}
+                                    handleDelete={handleDelete}
+                                    handleQuantityChange={handleQuantityChange}
+                                />;
                             })}
                         </table>
                     )}
