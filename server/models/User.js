078d063bad3aee5ac11bd646d707937f8b38f9fa
hyeyeonkim <hkim7963@gmail.com>
2023-08-02 03:03:41 -0700

    fix: server/index.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,40 +1,57 @@
 import mongoose from "mongoose";
+
 const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema;
 
 const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      trim: true,
-      required: true,
+    {
+        firstName: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        images: {
+            type: String,
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+            max: 64,
+        },
+        address: {
+            type: String,
+            trim: true,
+        },
+        role: {
+            type: Number,
+            default: 0,
+        },
+        googleId: {
+            type: String,
+            required: true,
+        },
+        displayName: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    lastName: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    email: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 6,
-      max: 64,
-    },
-    address: {
-      type: String,
-      trim: true,
-    },
-    role: {
-      type: Number,
-      default: 0,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model("User", userSchema);
---
+++ server/routes/authGoogle.js
@@ -0,0 +1,39 @@
+import express from "express";
+// const express = require("express");
+import passport from "passport";
+const router = express.Router();
+
+// middlewares
+import { requireSignin, isAdmin } from "../middlewares/auth.js";
+// controllers
+import { register, login, secret } from "../controllers/auth.js";
+
+router.get("/", (req, res) => {
+    res.render("login");
+    console.log(`여기는 "/auth"입니다!`);
+});
+
+router.get(
+    "/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { failureRedirect: "/" }),
+    (req, res) => {
+        res.redirect("/index/log");
+    }
+);
+
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+        res.redirect("http://localhost:8000");
+    });
+});
+
+export default router;
