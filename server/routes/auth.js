5936b9e33013ccb70a3e24983b5cd81bed4b8e85
hyeyeonkim <hkim7963@gmail.com>
2023-08-06 07:40:39 -0700

    feat: UserProfile

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,16 +5,26 @@ const router = express.Router();
 // middlewares
 import { requireSignin, isAdmin } from "../middlewares/auth.js";
 // controllers
-import { register, login, secret } from "../controllers/auth.js";
+import {
+    register,
+    login,
+    secret,
+    updateProfile,
+    getOrders,
+    allOrders,
+} from "../controllers/auth.js";
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/profile", requireSignin, updateProfile);
 router.get("/auth-check", requireSignin, (req, res) => {
-  res.json({ ok: true });
+    res.json({ ok: true });
 });
 router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
-  res.json({ ok: true });
+    res.json({ ok: true });
 });
+router.get("/orders", requireSignin, getOrders);
+router.get("/all-orders", requireSignin, isAdmin, allOrders);
 router.get("/secret", requireSignin, isAdmin, secret);
 
 export default router;
