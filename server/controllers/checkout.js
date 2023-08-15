75125170f3ef0b73c2ed00cacc2e26163569c133
hyeyeonkim <hkim7963@gmail.com>
2023-08-14 18:54:57 -0700

    fix: Indentation set to 4 spaces

--- server/controllers/checkout.js
+++ server/controllers/checkout.js
@@ -6,60 +6,60 @@ dotenv.config();
 
 // braintree config
 const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+    environment: braintree.Environment.Sandbox,
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
 
 export const getToken = async (req, res) => {
-  try {
-    await gateway.clientToken.generate({}, function (err, response) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(response); // token to show the drop-in UI
-      }
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        await gateway.clientToken.generate({}, function (err, response) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.send(response); // token to show the drop-in UI
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export const processPayment = async (req, res) => {
-  try {
-    const { nonce, cart, cartQuantity } = req.body;
+    try {
+        const { nonce, cart, cartQuantity } = req.body;
 
-    let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-      total += cart[i].price * cartQuantity[cart[i]._id];
-    }
-    total = total.toFixed(2);
-
-    let newTransaction = gateway.transaction.sale(
-      {
-        amount: total,
-        paymentMethodNonce: nonce,
-        options: {
-          submitForSettlement: true, //immediate settlement
-        },
-      },
-      async function (error, result) {
-        if (result) {
-          const order = new Order({
-            products: cart,
-            payment: result,
-            buyer: req.user._id,
-          });
-          console.log(order);
-          const savedOrder = await order.save();
-          res.json({ ok: true, orderId: savedOrder._id });
-        } else {
-          res.status(500).send(error);
+        let total = 0;
+        for (let i = 0; i < cart.length; i++) {
+            total += cart[i].price * cartQuantity[cart[i]._id];
         }
-      }
-    );
-  } catch (err) {
-    console.log(err);
-  }
+        total = total.toFixed(2);
+
+        let newTransaction = gateway.transaction.sale(
+            {
+                amount: total,
+                paymentMethodNonce: nonce,
+                options: {
+                    submitForSettlement: true, //immediate settlement
+                },
+            },
+            async function (error, result) {
+                if (result) {
+                    const order = new Order({
+                        products: cart,
+                        payment: result,
+                        buyer: req.user._id,
+                    });
+                    console.log(order);
+                    const savedOrder = await order.save();
+                    res.json({ ok: true, orderId: savedOrder._id });
+                } else {
+                    res.status(500).send(error);
+                }
+            }
+        );
+    } catch (err) {
+        console.log(err);
+    }
 };
--- server/models/product.js
+++ server/models/product.js
@@ -2,31 +2,31 @@ import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
 
 const productSchema = new mongoose.Schema(
-  {
-    title: { type: String, trim: true, required: true, maxlength: 160 },
-    slug: { type: String, lowercase: true },
-    downloadUrl: { type: String, trim: true, required: true },
-    images: { data: Buffer, contentType: String },
-    category: { type: ObjectId, ref: "Category", required: true },
-    ageCategory: { type: ObjectId, ref: "ageCategory", required: true },
-    description: { type: {}, required: true, maxlength: 2000 },
-    price: { type: Number, required: true },
-    reviewRate: {
-      type: Number,
-      required: false,
-      default: 0,
-      get: (value) => value.toFixed(2),
-      set: (value) => parseFloat(value.toFixed(2)),
+    {
+        title: { type: String, trim: true, required: true, maxlength: 160 },
+        slug: { type: String, lowercase: true },
+        downloadUrl: { type: String, trim: true, required: true },
+        images: { data: Buffer, contentType: String },
+        category: { type: ObjectId, ref: "Category", required: true },
+        ageCategory: { type: ObjectId, ref: "ageCategory", required: true },
+        description: { type: {}, required: true, maxlength: 2000 },
+        price: { type: Number, required: true },
+        reviewRate: {
+            type: Number,
+            required: false,
+            default: 0,
+            get: (value) => value.toFixed(2),
+            set: (value) => parseFloat(value.toFixed(2)),
+        },
+        reviewNumber: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        numberSold: { type: Number, required: false, default: 0 },
+        createdAt: { type: Date, default: Date.now, required: true },
     },
-    reviewNumber: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
-    numberSold: { type: Number, required: false, default: 0 },
-    createdAt: { type: Date, default: Date.now, required: true },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export default mongoose.model("Product", productSchema);
