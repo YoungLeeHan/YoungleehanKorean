1de83a5993bf536dede7282586721e5c29775633
Young <hyangyoungs@gmail.com>
2023-08-15 13:04:09 -0500

    add: blog comment likes

--- server/controllers/blogComment.js
+++ server/controllers/blogComment.js
@@ -1,9 +1,18 @@
-import slugify from "slugify";
 import blogComment from "../models/blogComment.js";
+import mongoose from "mongoose";
+
+
+const validateMongodbId = id => {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new Error("The id is not valid or found");
+};
+
 
 export const create = async (req, res) => {
+    console.log("create comment")
+
     const user = req.user;
-    const { postId, description} = req.body;
+    const {postId, description} = req.body;
     try {
         const comment = await blogComment.create({
             post: postId,
@@ -17,42 +26,9 @@ export const create = async (req, res) => {
     }
 };
 
-
-
-
-export const update = async (req, res) => {
-    try {
-        const { name } = req.body;
-        const { categoryId } = req.params;
-        console.log(name, categoryId);
-        const category = await BlogCategory.findByIdAndUpdate(
-            categoryId,
-            {
-                name,
-                slug: slugify(name),
-            },
-            { new: true }
-        );
-        res.json(category);
-    } catch (err) {
-        return res.status(400).json(err.message);
-    }
-};
-
-export const remove = async (req, res) => {
-    try {
-        const removed = await BlogCategory.findByIdAndDelete(
-            req.params.categoryId
-        );
-        res.json(removed);
-    } catch (err) {
-        return res.status(400).json(err.message);
-    }
-};
-
 export const list = async (req, res) => {
     try {
-        const all = await BlogCategory.find({});
+        const all = await blogComment.find({}).sort("-created");
         res.json(all);
     } catch (err) {
         return res.status(400).json(err.message);
@@ -60,10 +36,84 @@ export const list = async (req, res) => {
 };
 
 export const read = async (req, res) => {
+    const {id} = req.params
+    validateMongodbId(id)
     try {
-        const category = await BlogCategory.findOne({ slug: req.params.slug });
-        res.json(category);
+        const comment = await blogComment.findById(id);
+        res.json(comment);
     } catch (err) {
         return res.status(400).json(err.message);
     }
 };
+
+
+export const update = async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id)
+
+    try {
+        const update = await blogComment.findByIdAndUpdate(id, {
+                post: req.body?.postId,
+                user: req?.user,
+                description: req?.body?.description,
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+        res.json(update)
+    } catch (err) {
+        return res.status(400).json(err.message);
+    }
+};
+
+
+
+
+export const remove = async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id)
+
+    try {
+        const removed = await blogComment.findByIdAndDelete(id);
+        res.json(removed);
+    } catch (err) {
+        return res.status(400).json(err.message);
+    }
+};
+
+
+
+export const like = async (req, res) => {
+
+    const { id } = req.body;
+    const comment = await blogComment.findById(id)
+
+    const loginUserId = req?.user?._id;
+    const isLiked = comment?.isLiked;
+
+    if (isLiked) {
+        const comment = await blogComment.findByIdAndUpdate(
+            postId,
+            {
+                $pull: { likes: loginUserId },
+                isLiked: false,
+            },
+            { new: true }
+        );
+        res.json(comment);
+    } else {
+        //add to likes
+        const comment = await blogComment.findByIdAndUpdate(
+            postId,
+            {
+                $push: { likes: loginUserId },
+                isLiked: true,
+            },
+            { new: true }
+        );
+        res.json(comment);
+    }
+};
+
--- server/routes/blogComment.js
+++ server/routes/blogComment.js
@@ -3,12 +3,34 @@ import express from "express";
 const router = express.Router();
 
 // middlewares
-import { requireSignin, isAdmin } from "../middlewares/auth.js";
+import { requireSignin } from "../middlewares/auth.js";
 // controllers
 import {
     create,
+    list,
+    read,
+    update,
+    remove,
+    like,
 } from "../controllers/blogComment.js";
 
-router.post("/create", requireSignin, isAdmin, create);
+// 2. 특정 포스팅에 대한 코멘트 작성하기
+// axios.post(`/blog/comment/create/${post._id}`, { user: {_id, name, ...}, content });
+router.post("/", requireSignin, create);
+// 1. 해당 포스팅에 대한 코맨트 리스트 가져오기     (내가 적었는지 확인할 수 있는 variable 필요)
+// axios.get(`/blog/comment/list/${post._id}`);
+router.get("/", requireSignin, list);
+router.get("/:id", requireSignin, read);
+// 4. 자신이 적은 코멘트 수정하기
+// axios.put(`/blog/comment/change/${comment._id}`, {content})
+router.put("/:id", requireSignin, update);
+// 5. 자신이 적은 코멘트 삭제하기
+// axios.delete(`blog/comment/deletion/${comment._id}`)
+router.delete("/:id", requireSignin, remove);
+// 3. 좋아요 버튼 누르면 likeCount 정보 수정하기
+// axios.put(`/blog/comment/likeCount/${comment._id}`, {likeCount});
+router.put("/likes", requireSignin, like);
+
+
 
 export default router;
