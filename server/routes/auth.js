10a5b4efd34a080561e5c2ee35de63c53228ac51
Younghyun Lee <younghyunlee22@gmail.com>
2023-08-16 23:11:30 -0500

    feat: emailing via nodemailer

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,16 +5,23 @@ const router = express.Router();
 // middlewares
 import { requireSignin, isAdmin } from "../middlewares/auth.js";
 // controllers
-import { register, login, secret, updateProfile } from "../controllers/auth.js";
+import {
+  register,
+  login,
+  secret,
+  updateProfile,
+  sendCode,
+} from "../controllers/auth.js";
 
 router.post("/register", register);
+router.post("/send-code", sendCode);
 router.post("/login", login);
 router.put("/profile", requireSignin, updateProfile);
 router.get("/auth-check", requireSignin, (req, res) => {
-    res.json({ ok: true });
+  res.json({ ok: true });
 });
 router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
-    res.json({ ok: true });
+  res.json({ ok: true });
 });
 router.get("/secret", requireSignin, isAdmin, secret);
 
