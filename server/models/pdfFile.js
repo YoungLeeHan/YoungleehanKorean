0d4b3b5dfd61246d940d364b23bda82d6b63f1c3
Younghyun Lee <younghyunlee22@gmail.com>
2023-08-31 08:19:51 -0500

    feat: pdf upload

---
+++ server/models/pdfFile.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const pdfFileSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: [true, "Uploaded file must have a name"],
+  },
+});
+
+export default mongoose.model("pdfFile", pdfFileSchema);
--- server/models/product.js
+++ server/models/product.js
@@ -2,35 +2,31 @@ import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
 
 const productSchema = new mongoose.Schema(
-    {
-        title: { type: String, trim: true, required: true, maxlength: 160 },
-        slug: { type: String, lowercase: true },
-        // prev img
-        images: { data: Buffer, contentType: String },
-        // new img
-        // thumbnail: { type: String },
-        uploadedImagesPath: [{ type: String }],
-        //
-        category: { type: ObjectId, ref: "Category", required: true },
-        ageCategory: { type: ObjectId, ref: "ageCategory", required: true },
-        description: { type: {}, required: true, maxlength: 2000 },
-        price: { type: Number, required: true },
-        reviewRate: {
-            type: Number,
-            required: false,
-            default: 0,
-            get: (value) => value.toFixed(2),
-            set: (value) => parseFloat(value.toFixed(2)),
-        },
-        reviewNumber: {
-            type: Number,
-            required: false,
-            default: 0,
-        },
-        numberSold: { type: Number, required: false, default: 0 },
-        createdAt: { type: Date, default: Date.now, required: true },
+  {
+    title: { type: String, trim: true, required: true, maxlength: 160 },
+    slug: { type: String, lowercase: true },
+    pdfPath: { type: String },
+    uploadedImagesPath: [{ type: String }],
+    category: { type: ObjectId, ref: "Category", required: true },
+    ageCategory: { type: ObjectId, ref: "ageCategory", required: true },
+    description: { type: {}, required: true, maxlength: 2000 },
+    price: { type: Number, required: true },
+    reviewRate: {
+      type: Number,
+      required: false,
+      default: 0,
+      get: (value) => value.toFixed(2),
+      set: (value) => parseFloat(value.toFixed(2)),
     },
-    { timestamps: true }
+    reviewNumber: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    numberSold: { type: Number, required: false, default: 0 },
+    createdAt: { type: Date, default: Date.now, required: true },
+  },
+  { timestamps: true }
 );
 
 export default mongoose.model("Product", productSchema);
